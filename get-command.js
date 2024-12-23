3d5fc505038ebd84ee4d816c1470c4f1949a7651
Haozhe Li <lihaozhe013@outlook.com>
2024-12-22 22:04:25 -0500

    finished everything for 1st ver

--- get-command.js
+++ get-command.js
@@ -1,22 +1,49 @@
-const select_mode = document.querySelector('input[name="select-mode"]:checked');
-
+let inputFileName = "";
 function nevigate2SelectedMode() {
+    const select_mode = document.querySelector('input[name="select-mode"]:checked');
     if (select_mode) {
-        window.location.href = select_mode.value;
+        if (select_mode.id == "copy") {
+            window.location.href = "copy-mode.html";
+        }
+        else if (select_mode.id == "transcode") {
+            window.location.href = "transcode-mode.html";
+        }
     }
-    
+    else alert('Please select a option!!!');
+}
+
+function goBack() {
+    window.location.href = "index.html";
 }
 
 
-let output = "";
-let inputFileName = "";
-const outputBox = document.getElementById('output-box');
 function generateCopyCommand() {
-    // ffmpeg -i input.mkv -c copy output.mp4
-    output = "ffmpeg -i " + inputFileName + " -c copy output.mp4";
+    let output = "";
+    const outputBox = document.getElementById('output-box');
+    const fileName = document.getElementById("input-file-name");
+    const containerFormat = document.querySelector('input[name="select-container"]:checked');
+    output = "ffmpeg -i " + fileName.value + " -c copy output." + containerFormat.value;
     outputBox.textContent = output;
 }
 
+function generateTranscodeCommand() {
+    let output = "";
+    const outputBox = document.getElementById('output-box');
+    const fileName = document.getElementById("input-file-name");
+    const codingFormat = document.querySelector('input[name="select-coding-format"]:checked');
+    const containerFormat = document.querySelector('input[name="select-container"]:checked');
+    const transcodingQuality = document.getElementById("quality");
+    if (codingFormat.id == "av1") {
+        alert("the function is not finished yet, please try other format")
+    }
+    else if (codingFormat.id == "hvac") {
+        output = "ffmpeg -i " + fileName.value + " -c:v libx265 -crf " + transcodingQuality.value + " -preset slow -c:a aac -b:a 192k -movflags +faststart output." + containerFormat.value;
+    }
+    else if (codingFormat.id == "avc") {
+        output = "ffmpeg -i " + fileName.value + " -c:v libx264 -crf " + transcodingQuality.value + " -preset slow -c:a aac -b:a 192k -movflags +faststart output." + containerFormat.value;
+    }
+    outputBox.textContent = output;
+}
 
 function copyText() {
     const outputBox = document.getElementById('output-box');
@@ -36,6 +63,11 @@ function copyText() {
     document.body.removeChild(tempInput);
 }
 
-function generateCommand() {
-    generateCopyCommand();
+function generateCommand(modeID) {
+    if (modeID == 0) {
+        generateCopyCommand();
+    }
+    else {
+        generateTranscodeCommand();
+    }
 }
