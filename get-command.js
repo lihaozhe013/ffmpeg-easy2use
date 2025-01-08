535ccfa30da1af802ec11f1e647a3805e0b67bf6
Haozhe <lihaozhe013@outlook.com>
2025-01-08 18:51:35 -0500

    complete advanced mode

--- get-command.js
+++ get-command.js
@@ -30,12 +30,46 @@ function generateTranscodeCommand() {
     const outputBox = document.getElementById('output-box');
     const fileName = document.getElementById("input-file-name");
     const codingFormat = document.querySelector('input[name="select-coding-format"]:checked');
+    const audioQuality = document.querySelector('input[name="select-audio-quality"]:checked');
+    const fastOrSlow = document.querySelector('input[name="fast-or-slow-mode"]:checked');
+    const numberOfThreads = document.getElementById('threads');
     const containerFormat = document.querySelector('input[name="select-container"]:checked');
     const transcodingQuality = document.getElementById("quality");
-    if (transcodingQuality.value > 51 || transcodingQuality.value < 0) {
+    if (transcodingQuality.value > 51) {
+        transcodingQuality.value = 51;
         alert('Transcoding Quality should be at least 0, and cannot be more than 51');
     }
-    let output = "";
+    else if (transcodingQuality.value < 0) {
+        transcodingQuality.value = 0;
+        alert('Transcoding Quality should be at least 0, and cannot be more than 51');
+    }
+    let transcoder = "";
+    if (codingFormat.id === "av1") {
+        transcoder = "libaom-av1";
+    } else if (codingFormat.id === "hvac") {
+        transcoder = "libx265";
+    } else if (codingFormat.id === "avc") {
+        transcoder = "libx265";
+    }
+    let output = "ffmpeg -i " + fileName.value + " -c:v " + transcoder + " -crf " + transcodingQuality.value + " -preset " + fastOrSlow.value + " -c:a aac -b:a " + audioQuality.value + "k -movflags +faststart -threads " + numberOfThreads.value + " output." + containerFormat.value;
+    outputBox.textContent = output;
+}
+
+function generateSimpleTranscodeCommand() {
+    const simpleOutputBox = document.getElementById('output-box');
+    const fileName = document.getElementById("input-file-name");
+    const codingFormat = document.querySelector('input[name="select-coding-format"]:checked');
+    const containerFormat = document.querySelector('input[name="select-container"]:checked');
+    const transcodingQuality = document.getElementById("quality");
+    if (transcodingQuality.value > 51) {
+        transcodingQuality.value = 51;
+        alert('Transcoding Quality should be at least 0, and cannot be more than 51');
+    }
+    else if (transcodingQuality.value < 0) {
+        transcodingQuality.value = 0;
+        alert('Transcoding Quality should be at least 0, and cannot be more than 51');
+    }
+    let output;
     if (codingFormat.id === "av1") {
         // alert("the function is not finished yet, please try other format")
         output = "ffmpeg -i " + fileName.value + " -c:v libaom-av1 -crf " + transcodingQuality.value + " -preset slow -c:a aac -b:a 320k -movflags +faststart -threads 4 output." + containerFormat.value;
@@ -44,7 +78,7 @@ function generateTranscodeCommand() {
     } else if (codingFormat.id === "avc") {
         output = "ffmpeg -i " + fileName.value + " -c:v libx264 -crf " + transcodingQuality.value + " -preset slow -c:a aac -b:a 320k -movflags +faststart -threads 4 output." + containerFormat.value;
     }
-    outputBox.textContent = output;
+    simpleOutputBox.textContent = output;
 }
 
 function copyText() {
@@ -66,10 +100,13 @@ function copyText() {
 }
 
 function generateCommand(modeID) {
-    if (modeID === 0) {
+    if (modeID == 0) {
         generateCopyCommand();
     }
-    else {
+    else if (modeID == 1) {
+        generateSimpleTranscodeCommand();
+    }
+    else if (modeID == 2) {
         generateTranscodeCommand();
     }
 }
